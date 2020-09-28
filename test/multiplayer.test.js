090f294c58543a215168406574461b203c52df34
Wasya1212 <leonardo.1212@yandex.ua>
2020-09-28 17:14:01 +0300

    added room test

--- test/multiplayer.test.js
+++ test/multiplayer.test.js
@@ -1,5 +1,21 @@
 const { Room, Multiplayer } = require('../libs/multiplayer');
 
-test('Create room', () => {
-  expect((new Room('wasya1212', 'room', 'password', false, false)).name).toBe('wasya1212');
+test('Room client', () => {
+  expect(new Room('wasya1212').currentClientName).toBe('wasya1212');
+});
+
+test('Room members', () => {
+  expect(new Room().members).toBeUndefined();
+});
+
+test('Room members count', () => {
+  expect(new Room('wasya1212', 'room', 'password', 5).maxMembersCount).toBe(5);
+});
+
+test('Room owner', () => {
+  expect(new Room('wasya1212', 'room', 'password').isOwner).toBeTruthy();
+});
+
+test('Room owner replace', () => {
+  expect(new Room('wasya1212', 'room', 'password', null, false).isOwner).toBeFalsy();
 });
